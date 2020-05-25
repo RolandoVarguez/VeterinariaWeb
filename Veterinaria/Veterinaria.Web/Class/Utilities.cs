f4d9c4414a2a159935e7c2013530a5f0bceffb3e
RolandoVarguez <60460622+RolandoVarguez@users.noreply.github.com>
2020-05-25 14:06:29 -0700

    CREACION DE ROLES Y DISENO DE VISTAS

---
+++ Veterinaria/Veterinaria.Web/Class/Utilities.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Veterinaria.Web.Models;
+
+namespace Veterinaria.Web.Class
+{
+    public class Utilities
+    {
+        readonly static ApplicationDbContext db = new ApplicationDbContext();
+
+        public static void CheckRoles(string roleName)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+
+            if (!roleManager.RoleExists(roleName))
+            {
+                roleManager.Create(new IdentityRole(roleName));
+            }
+        }
+
+        internal static void CheckSuperUser()
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var userAsp = userManager.FindByName("rolando@admin.com");
+
+            if (userAsp == null)
+            {
+                CreateUserASP("rolando@admin.com", "rolando30", "Admin");
+            }
+        }
+
+        internal static void CheckClientDefault()
+        {
+            var clientdb = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var userclient = clientdb.FindByName("cliente@veterinary.com");
+            if (userclient == null)
+            {
+                CreateUserASP("cliente@veterinary.com", "cliente123", "Owner");
+            }
+
+        }
+
+
+        public static void CreateUserASP(string email, string password, string rol)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+            var userASP = new ApplicationUser()
+            {
+                UserName = email,
+                Email = email,
+            };
+
+            userManager.Create(userASP, password);
+            userManager.AddToRole(userASP.Id, rol);
+        }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+    }
+}
--- Veterinaria/Veterinaria.Web/Controllers/AccountController.cs
+++ Veterinaria/Veterinaria.Web/Controllers/AccountController.cs
@@ -8,6 +8,7 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using Veterinaria.Web.Class;
 using Veterinaria.Web.Models;
 
 namespace Veterinaria.Web.Controllers
@@ -147,15 +148,14 @@ namespace Veterinaria.Web.Controllers
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
+        public ActionResult Register(RegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
                 var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
-                var result = await UserManager.CreateAsync(user, model.Password);
-                if (result.Succeeded)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
+                Utilities.CreateUserASP(user.Email, model.Password, "Owner");
+              
+                    
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
@@ -164,8 +164,8 @@ namespace Veterinaria.Web.Controllers
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
                     return RedirectToAction("Index", "Home");
-                }
-                AddErrors(result);
+               
+               
             }
 
             // If we got this far, something failed, redisplay form
